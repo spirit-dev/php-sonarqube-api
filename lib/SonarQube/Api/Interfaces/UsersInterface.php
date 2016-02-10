b89d9b34e34c533cae6c9c51d323d653ea9061a6
Jean Bordat <bordat.jean@gmail.com>
2016-02-10 15:10:59 +0100

    Preparing Users interface

---
+++ lib/SonarQube/Api/Interfaces/UsersInterface.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright (c) 2016. Spirit-Dev
+ *    _             _
+ *   /_`_  ._._/___/ | _
+ * . _//_//// /   /_.'/_'|/
+ *    /
+ *
+ * By Jean Bordat ( Twitter @Ji_Bay_ )
+ * Since 2K10 until today
+ * @mail <bordat.jean@gmail.com>
+ *
+ * hex: 53 70 69 72 69 74 2d 44 65 76
+ */
+
+namespace SonarQube\Api\Interfaces;
+
+interface UsersInterface {
+
+    /**
+     * Create a user. Requires Administer System permission
+     * Type POST
+     * Parameters
+     *      Optional
+     *          - email                 User email
+     *          - scm_account           SCM accounts. This parameter has been added in 5.1
+     *      Required
+     *          - login                 User login
+     *          - name                  User name
+     *          - password              User password
+     *          - password_confirmation Must be the same value as "password"
+     *
+     * @return mixed
+     */
+    public function create();
+
+    /**
+     * Deactivate a user. Requires Administer System permission
+     * Type POST
+     * Parameters
+     *      Required
+     *          - login                 User login
+     *
+     * @return mixed
+     */
+    public function deactivate();
+
+    /**
+     * Get a list of users
+     * Type GET
+     * Parameters
+     *      Optional
+     *          - includeDeactivated    Include deactivated users
+     *          - logins                Comma-separated list of user logins
+     *
+     * @return mixed
+     */
+    public function search();
+
+    /**
+     * Update a user. Requires Administer System permission
+     * Type POST
+     * Parameters
+     *      Optional
+     *          - email                 User email
+     *          - scm_account           SCM accounts. This parameter has been added in 5.1
+     *      Required
+     *          - login                 User login
+     *          - name                  User name
+     *          - password              User password
+     *          - password_confirmation Must be the same value as "password"
+     *
+     * @return mixed
+     */
+    public function update();
+
+}
