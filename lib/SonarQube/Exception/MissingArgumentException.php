2231aef77064cc6a2354a9142ba7182dbc170763
Jean Bordat <bordat.jean@gmail.com>
2016-02-10 14:00:49 +0100

    Adding Comments

--- lib/SonarQube/Exception/MissingArgumentException.php
+++ lib/SonarQube/Exception/MissingArgumentException.php
@@ -1,11 +1,32 @@
 <?php
+/**
+ * Copyright (c) 2016. Spirit-Dev
+ *    _             _
+ *   /_`_  ._._/___/ | _
+ * . _//_//// /   /_.'/_'|/
+ *    /
+ *
+ * By Jean Bordat ( Twitter @Ji_Bay_ )
+ * Since 2K10 until today
+ * @mail <bordat.jean@gmail.com>
+ *
+ * hex: 53 70 69 72 69 74 2d 44 65 76
+ */
 
 namespace SonarQube\Exception;
 
+/**
+ * Class MissingArgumentException
+ * @package SonarQube\Exception
+ */
 class MissingArgumentException extends ErrorException {
 
-    // TODO Comment
-
+    /**
+     * MissingArgumentException constructor.
+     * @param string $required
+     * @param int $code
+     * @param null $previous
+     */
     public function __construct($required, $code = 0, $previous = null) {
         if (is_string($required)) {
             $required = array($required);
