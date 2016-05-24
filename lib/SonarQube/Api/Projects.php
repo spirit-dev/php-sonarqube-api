06e2c87cb33a157650a6c925b62363c32848a728
Jean Bordat <bordat.jean@gmail.com>
2016-05-24 20:34:45 +0200

    Updating sonar qube url destroy project for sonar 5.3

--- lib/SonarQube/Api/Projects.php
+++ lib/SonarQube/Api/Projects.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Copyright (c) 2016. Spirit-Dev
- *    _             _
- *   /_`_  ._._/___/ | _
+ * Copyright (c) 2016. Spirit-Dev 
+ *    _             _      
+ *   /_`_  ._._/___/ | _   
  * . _//_//// /   /_.'/_'|/
- *    /
- *
+ *    /                    
+ *    
  * By Jean Bordat ( Twitter @Ji_Bay_ )
  * Since 2K10 until today
  * @mail <bordat.jean@gmail.com>
- *
+ *  
  * hex: 53 70 69 72 69 74 2d 44 65 76
  */
 
@@ -42,8 +42,8 @@ class Projects extends AbstractApi implements ProjectsInterface {
      * @inheritDoc
      */
     public function deleteProject(array $parameters = array()) {
-        $this->checkParamIsSet($parameters, array("id"));
-        return $this->post('projects/destroy', $parameters);
+        $this->checkParamIsSet($parameters, array("key"));
+        return $this->post('projects/delete', $parameters);
     }
 
 }
