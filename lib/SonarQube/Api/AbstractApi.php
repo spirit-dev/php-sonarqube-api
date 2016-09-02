2789695f8b3a0e0f4fe17ba012093effbac81adf
Simon Champion <simonc@sfwltd.co.uk>
2016-09-02 12:27:30 +0100

    SonarQube API returns Content-Type:application/json;charset=utf-8. Charset can vary, but this wasn't being picked up as json by the Response class, so it wasn't doing json_decode(). Fixed that. I then had to fix the AbstractAPI to stop doing json_decode() as it isn't necessary there any more.

--- lib/SonarQube/Api/AbstractApi.php
+++ lib/SonarQube/Api/AbstractApi.php
@@ -37,31 +37,31 @@ abstract class AbstractApi {
     protected function get($path, array $parameters = array(), $requestHeaders = array()) {
         $response = $this->client->getHttpClient()->get($path, $parameters, $requestHeaders);
 
-        return json_decode($response->getContent(), true);
+        return $response->getContent();
     }
 
     protected function post($path, array $parameters = array(), $requestHeaders = array()) {
         $response = $this->client->getHttpClient()->post($path, $parameters, $requestHeaders);
 
-        return json_decode($response->getContent(), true);
+        return $response->getContent();
     }
 
     protected function patch($path, array $parameters = array(), $requestHeaders = array()) {
         $response = $this->client->getHttpClient()->patch($path, $parameters, $requestHeaders);
 
-        return json_decode($response->getContent(), true);
+        return $response->getContent();
     }
 
     protected function put($path, array $parameters = array(), $requestHeaders = array()) {
         $response = $this->client->getHttpClient()->put($path, $parameters, $requestHeaders);
 
-        return json_decode($response->getContent(), true);
+        return $response->getContent();
     }
 
     protected function delete($path, array $parameters = array(), $requestHeaders = array()) {
         $response = $this->client->getHttpClient()->delete($path, $parameters, $requestHeaders);
 
-        return json_decode($response->getContent(), true);
+        return $response->getContent();
     }
 
     protected function encodePath($path) {
